d8a0c9fb9c1535978e54bff9540dab376f09ef00
Chintan Sheth <chintz604@gmail.com>
2020-07-13 11:46:17 +0530

    2020-07-13 0500hr IST nowcast

--- landslide_nowcast_pipeline.R
+++ landslide_nowcast_pipeline.R
@@ -2,14 +2,20 @@ library(cleangeo)
 library(raster)
 library(rgdal)
 library(dplyr)
-library(tibble)
+library(lubridate)
 
-landslide <- readOGR("")
+## paste the various strings to create the url for UTC 2330 hrs = IST 0500 hrs. Add the Julian Day likewise. 
+## As the release of the 2330 hrs nowcast is one day later in UTC subtract one day from todays date
 
-circles <- readOGR("")
+landslide_url <-  paste0("https://pmmpublisher.pps.eosdis.nasa.gov/products/global_landslide_nowcast_3hr/export/Global/",format(now(tzone="UTC")-days(1), "%Y"), "/", format(now(tzone="UTC")-days(1), "%j"), "/", "global_landslide_nowcast_3hr.", format(now(tzone="UTC")-days(1), "%Y%m%d"), ".233000.geojson")
+
+download.file(landslide_url, destfile = basename(landslide_url))
+
+landslide <- readOGR(paste0(basename(landslide_url)))
+
+circles <- readOGR("arunachal-pradesh-circles.geojson")
  
 landslide_cleaned <- clgeo_Clean(landslide, errors.only = NULL, strategy = "POLYGONATION", verbose = FALSE)
-
 
 intersectit <- raster::intersect(landslide_cleaned, circles)
 
@@ -17,4 +23,3 @@ landslide_circles <- as.data.frame(intersectit)
 
 table_landslide_circles <- landslide_circles %>% select(circle, district) %>% arrange(district) %>% distinct(circle, district)
 
-writeOGR(intersectit, dsn="intersection_landslide_nowcast_202007120500_arunachal_circles.GeoJSON", layer="intersectit", driver="GeoJSON")
--- script.js
+++ script.js
@@ -52,11 +52,11 @@ $.getJSON("arunachal-pradesh-circles.geojson", function (data) {
 
 // });
 
-$.getJSON("global_landslide_nowcast_3h_20200712-0500-ist.geojson", function (data) {
+$.getJSON("global_landslide_nowcast_3h.20200712.23300.geojson", function (data) {
  geoJsonLayer = L.geoJson(data, {
     style: style
       }).addTo(map);
-controlLayers.addOverlay(geoJsonLayer, 'Landslide Nowcast 0500 hrs IST 12th July 2020');
+controlLayers.addOverlay(geoJsonLayer, 'Landslide Nowcast 0500 hrs IST 13th July 2020');
 
 });
 
